f90e514d84dd45f251e510e38aff4629a37786d1
Sean Fisher <srtfisher@gmail.com>
2013-09-26 19:58:34 -0400

    Maybe not

--- json-api.php
+++ json-api.php
@@ -10,8 +10,7 @@ Author URI: http://phiffer.org/
 
 use JsonApi\Manager\Application,
 	JsonApi\Manager\Settings;
-	
-$dir = json_api_dir();
+
 if (! file_exists($dir.'/vendor/autoload.php')) :
 	echo "Composer not setup for REST API";
 	return;
@@ -19,31 +18,15 @@ else :
 	require_once $dir.'/vendor/autoload.php';
 endif;
 
-/*
-require_once $dir . '/singletons/api.php';
-require_once $dir . '/singletons/query.php';
-require_once $dir . '/singletons/introspector.php';
-require_once $dir . '/singletons/response.php';
-require_once $dir . '/models/post.php';
-require_once $dir . '/models/comment.php';
-require_once $dir . '/models/category.php';
-require_once $dir . '/models/tag.php';
-require_once $dir . '/models/author.php';
-require_once $dir . '/models/attachment.php';
-*/
 /**
  * Initialize the Plugin
  * 
  * @return void
+ * @access private
  */
 function json_api_init() {
-	global $json_api;
-
 	if (phpversion() < 5.3)
 		return add_action('admin_notices', 'json_api_php_version_warning');
-
-	//if (! class_exists('Application'))
-	//	return add_action('admin_notices', 'json_api_class_warning');
 
 	add_filter('rewrite_rules_array', 'json_api_rewrites');
 
@@ -57,15 +40,6 @@ function json_api_init() {
  */
 function json_api_php_version_warning() {
 	echo "<div id=\"json-api-warning\" class=\"updated fade\"><p>Sorry, JSON API requires PHP version 5.0 or greater.</p></div>";
-}
-
-/**
- * Warning when JSON API is setup incorrectly
- * 
- * @return void
- */
-function json_api_class_warning() {
-	echo "<div id=\"json-api-warning\" class=\"updated fade\"><p>Oops, JSON_API class not found. If you've defined a JSON_API_DIR constant, double check that the path is correct.</p></div>";
 }
 
 /**
@@ -101,30 +75,22 @@ function json_api_deactivation() {
 function json_api_rewrites($wp_rules) {
 	$settings = Settings::Instance();
 
-	if (! $settings->base) return;
-	$base = $settings->base;
+	if (! $settings->base)
+		$base = 'api';
+	else
+		$base = $settings->base;
 
 	if (empty($base)) {
 		return $wp_rules;
 	}
 	$json_api_rules = array(
-		"$base\$" => 'index.php?json=info',
-		"$base/(.+)\$" => 'index.php?json=$matches[1]'
+		"$base\$" => 'index.php?apiRequest=Coreinfo',
+		"$base/(.+)\$" => 'index.php?apiRequest=$matches[1]'
 	);
+
 	return array_merge($json_api_rules, $wp_rules);
 }
 
-/**
- * Directory of the JSON API Plugin
- *
- * You can override the JSON API Directory by setting the
- * constant 'JSON_API_DIR'.
- * 
- * @return string
- */
-function json_api_dir() {
-	return (defined('JSON_API_DIR') && file_exists(JSON_API_DIR)) ? JSON_API_DIR : __DIR__;
-}
 
 /**
  * Setup the Default Controllers
@@ -136,7 +102,6 @@ function wp_rest_api_default_controllers($collection)
 	$collection->register(new JsonApi\Controller\Core);
 	$collection->register(new JsonApi\Controller\Posts);
 }
-
 add_action('wp-rest-api-controllers', 'wp_rest_api_default_controllers');
 
 // Add initialization and activation hooks
