4d035f5bee0b8c7fd2792fcb88b9d3577b10b49a
Sean Fisher <srtfisher@gmail.com>
2013-09-26 18:47:03 -0400

    Coll

---
+++ src/JsonApi/Manager/Collection.php
@@ -0,0 +1,60 @@
+<?php namespace JsonApi\Manager;
+
+use JsonApi\Controller\ControllerInterface;
+
+/**
+ * Manager the controllers and store them all
+ *
+ * To register a controller, look at the `wp-rest-api-controllers`
+ * hook. It will be called with an argument of the `Collection` class.
+ *
+ * Register it via the `register` method.
+ */
+class Collection {
+	protected $controllers;
+
+	/**
+	 * Store Controllers
+	 * 
+	 * @param array
+	 */
+	public function __construct($controllers = array())
+	{
+		$this->controllers = $controllers;
+
+		do_action('wp-rest-api-controllers', $this);
+	}
+
+	/**
+	 * Register a controller
+	 * 
+	 * @param object Controller Object: Intance of ControllerInterface
+	 */
+	public function register($controller)
+	{
+		if (! ( $controller instanceof ControllerInterface ))
+			wp_die(sprintf('Controller %s is not an instance of ControllerInterface', $controller->base));
+
+		$this->controllers[$controller->base] = $controller;
+	}
+
+	/**
+	 * Deregister a Controller
+	 *
+	 * @param  string
+	 */
+	public function deregister($name)
+	{
+		unset($this->controllers[$name]);
+	}
+
+	/**
+	 * Retrieve all controllers
+	 * 
+	 * @return array
+	 */
+	public function getControllers()
+	{
+		return $this->controllers;
+	}
+}
