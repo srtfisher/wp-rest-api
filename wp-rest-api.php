0995a0b091603d04102364eb40b4147e1657f43b
Sean Fisher <srtfisher@gmail.com>
2013-10-26 13:25:10 -0400

    Namespacing

--- wp-rest-api.php
+++ wp-rest-api.php
@@ -48,10 +48,10 @@ function json_api_php_version_warning() {
  */
 function wp_rest_api_default_controllers($collection)
 {
-	$collection->register(new WpRest\Controller\Core);
-	$collection->register(new WpRest\Controller\Posts);
-	$collection->register(new WpRest\Controller\Pages);
-	$collection->register(new WpRest\Controller\Categories);
-	$collection->register(new WpRest\Controller\Tags);
+	$collection->register(new \WpRest\Controller\Core);
+	$collection->register(new \WpRest\Controller\Posts);
+	$collection->register(new \WpRest\Controller\Pages);
+	$collection->register(new \WpRest\Controller\Categories);
+	$collection->register(new \WpRest\Controller\Tags);
 }
 add_action('wp-rest-api-controllers', 'wp_rest_api_default_controllers');
